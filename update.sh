1c1dbea401a90c8ce9e7ace701ff2e0e026e4701
Ryan Buxton <rbuxton1@gmail.com>
2019-12-21 18:29:01 -0700

    added a nice little update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+echo "Stoping any running instances. . ."
+docker-compose stop
+echo "DONE!"
+echo "- - - - - - - - - - - - - - - - - -"
+echo "Updating from git . . ."
+git pull
+echo "DONE!"
+echo "- - - - - - - - - - - - - - - - - -"
+echo "Starting detached, forcing a new build. . ."
+docker-compose up --build -d
